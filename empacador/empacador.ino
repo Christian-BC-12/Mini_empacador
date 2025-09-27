c88a4ce40f95b2e5fd4397675a197e0fdaf91ec2
Christian-BC-12 <christiancamberos12@gmail.com>
2025-09-26 20:47:18 -0600

    Update empacador.ino

--- empacador/empacador.ino
+++ empacador/empacador.ino
@@ -1,101 +1,94 @@
 //https://youtube.com/shorts/GqWufEbYm3c proyecto funcionando 
-#include <LiquidCrystal.h>
-#include <Servo.h>
-int motor = 9;
-int sensor1 = 10;
-int sensor2 = 11; 
-int sensor3 = 8;
-int led1 = 12;
-int led2 = 13;      
-int  valorIN = 0;
-int contenido = 0;
-LiquidCrystal lcd(1, 2, 4 ,5 ,6, 7); 
+ #include <Servo.h>
+int sensor2=7;
+int sensor=6;
+int led1 = 8;
+int led2 = 9;
+int sol1 = 10;
+int sol2 = 1;
+int boton = 11;
+int led3 = 13;
+int baseg = 12;
 Servo servo1;
 Servo servo2;
 Servo servo3;
 Servo servo4;
-Servo servo5;
 
-void setup() 
-{
-pinMode(sensor1,INPUT);
-pinMode(sensor2,INPUT);
-pinMode(sensor3,INPUT);
-pinMode(motor,OUTPUT); 
-pinMode(led1,OUTPUT);
-pinMode(led2,OUTPUT);
-servo1.attach(22);
-servo2.attach(24);
-servo3.attach(26);
-servo4.attach(28);
-servo5.attach(30);
-lcd.begin(16,2); 
-lcd.print("OBJETOS EN LA");
-lcd.setCursor(0,1);
-lcd.print("CAJA:");
-delay(1000);
+void setup() {
+  servo1.attach(2);
+  servo2.attach(3);
+  servo3.attach(4);
+  servo4.attach(5);
+  pinMode(sensor,INPUT);
+  pinMode(sensor2,INPUT);
+  pinMode(boton,INPUT);
+  pinMode(led1,OUTPUT);
+  pinMode(led2,OUTPUT);
+  pinMode(led3,OUTPUT);
+  pinMode(sol1,OUTPUT);
+  pinMode(sol2,OUTPUT);
+  pinMode(baseg,OUTPUT);
+
 }
-
+  
 void loop() 
 {
-if(digitalRead(sensor1) == LOW)
-{
-digitalWrite(motor,LOW);
-digitalWrite(led1,HIGH);
-digitalWrite(led2,LOW);
+if(digitalRead(boton) == HIGH)
+{  
+digitalWrite(sol2,HIGH);
+digitalWrite(led3,HIGH);
+digitalWrite(sol1,LOW);
+delay(100);
 }
-
-if(digitalRead(sensor2) == LOW)
-{
-digitalWrite(led2,HIGH);
-digitalWrite(led1,LOW);
-digitalWrite(motor,HIGH);
+else
+{    
+digitalWrite(led3,LOW);
+digitalWrite(sol1,HIGH);
+delay(100);
+}
+if(digitalRead(sensor) == LOW)         
+{  
+digitalWrite(sol1,HIGH);
+digitalWrite(sol2,LOW);
+digitalWrite(led2,LOW);
+delay(100);
+digitalWrite(led1,HIGH);
 delay(1000);
-servo1.write(120);
-servo2.write(105);
+servo2.write(100);
 delay(1500);
-servo3.write(100);
+servo3.write(90);
 delay(1500);
 servo4.write(0);
 delay(1000);
-servo3.write(160);
+servo3.write(140);
 delay(1000);
-servo2.write(100);
+servo2.write(60);
 delay(1000);
 servo1.write(175);
 delay(1000);
-servo2.write(115);
-delay(1000);
-servo3.write(150);
-delay(1000);
-servo4.write(100);
-delay(1000);   
-
-}
-if(digitalRead(sensor3) == LOW)
-{
-servo5.write(150);
- delay(1000);
-servo3.write(170);
-delay(1000);
 servo2.write(100);
+delay(1000);
+servo3.write(110);
+delay(1000);
+servo4.write(90);
+delay(1000);
+}
+if(digitalRead(sensor2) == LOW)
+{
+servo3.write(140);
+delay(1000);
+servo2.write(60);
 delay(1000);
 servo1.write(100);
 delay(1000);
- 
- }
- else
- {
-servo5.write(0);
- }
-valorIN=digitalRead(sensor3);
-if(valorIN!=1)
+digitalWrite(led1,LOW);
+digitalWrite(led2,HIGH);
+digitalWrite(baseg,HIGH);
+delay(200);
+digitalWrite(baseg,LOW);
+}
+else
 {
-contenido++;
-while(digitalRead(sensor3)!=1){
- delay(100);
- }
-lcd.setCursor(12,1);
-+lcd.print(contenido);
+digitalWrite(baseg,LOW);
 }
 }
